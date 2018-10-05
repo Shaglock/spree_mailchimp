f149dff6d071ad71d43641848d099a96a86960c3
Ilya Shaplyko <goldilb10xa@mail.ru>
2018-10-05 15:23:54 +0300

    Update user_decorator.rb

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,7 +1,5 @@
 module Spree
   PermittedAttributes.user_attributes << :mailchimp_opt_in
 
-  User.class_eval do
-    include SyncMailchimpPreferences
-  end
+  Spree::User.include(SyncMailchimpPreferences)
 end
