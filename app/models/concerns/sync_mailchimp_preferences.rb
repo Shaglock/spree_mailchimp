938c0cda1aa8756ed1bba3cb2d86b93c898f4ff3
Ilya Shaplyko <goldilb10xa@mail.ru>
2018-10-05 14:50:44 +0300

    Update sync_mailchimp_preferences.rb

--- app/models/concerns/sync_mailchimp_preferences.rb
+++ app/models/concerns/sync_mailchimp_preferences.rb
@@ -1,9 +1,27 @@
-module User
-  module SyncMailchimpPreferences
-    extend ActiveSupport::Concern
+module SyncMailchimpPreferences
+  extend ActiveSupport::Concern
 
-    included do
-      after_save :sync_mailchimp_preferences
+  included do
+    after_save :sync_mailchimp_preferences
+    
+    private 
+    def sync_mailchimp_preferences
+      if mailchimp_list
+        if mailchimp_opt_in
+          if new_record? || mailchimp_opt_in_changed?
+            mailchimp_list.subscribe(self)
+          else
+            mailchimp_list.update(self)
+          end
+        elsif !mailchimp_opt_in && !new_record? && mailchimp_opt_in_changed?
+          mailchimp_list.unsubscribe(self)
+        end
+      end
     end
+
+
+    def mailchimp_list
+      @mailchimp_list ||= SpreeMailchimp::List.new
+    end    
   end
 end
